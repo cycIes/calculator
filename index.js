e1ead2b1f95ea089c907bf2d4d597dadb02d9277
cycIes <79021718+cycIes@users.noreply.github.com>
2025-07-30 00:02:28 +0200

    Create operate function

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let num1;
+let num2;
+let operator;
+
 function add(a, b) {
     return a + b;
 }
@@ -14,7 +18,29 @@ function divide(a, b) {
     return a / b;
 }
 
-console.log(add(1, 5));
-console.log(subtract(1, 5));
-console.log(multiply(2, 5));
-console.log(divide(10, 5));
+function operate(num1, num2, operator) {
+    switch(operator) {
+        case "+":
+            return add(num1, num2);
+            break;
+        case "-":
+            return subtract(num1, num2);
+            break;
+        case "*":
+            return multiply(num1, num2);
+            break;
+        case "/":
+            return divide(num1, num2);
+            break;
+    }
+}
+
+// console.log(add(1, 5));
+// console.log(subtract(1, 5));
+// console.log(multiply(2, 5));
+// console.log(divide(10, 5));
+
+console.log(operate(1, 5, "+"));
+console.log(operate(1, 5, "-"));
+console.log(operate(2, 5, "*"));
+console.log(operate(10, 5, "/"));
