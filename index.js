6a93e25e9758ba6aef36df7d59db0099653acaac
cycIes <79021718+cycIes@users.noreply.github.com>
2025-07-29 23:57:23 +0200

    Create files and basic operation functions

---
+++ index.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(add(1, 5));
+console.log(subtract(1, 5));
+console.log(multiply(2, 5));
+console.log(divide(10, 5));
